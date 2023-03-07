065c6c9dc14e861084d6e9b621c27ce3f6e42879
theophilus doku <theodoku89@gmail.com>
2023-03-07 23:44:58 +0000

    modified myconfig.js and packages

---
+++ myconfig.js
@@ -0,0 +1,19 @@
+module.exports = {
+	ci: {
+		collect: {
+			numberOfRuns: 3,
+			startServerCommand: 'yarn run start',
+			url: ['http://localhost'],
+		},
+		assert: {
+			preset: 'lighthouse:recommended',
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
+  
+ 
+
+
