1d7efeedd1314ede160c457b861840060f0db177
theophilus doku <theodoku89@gmail.com>
2023-03-09 07:40:25 +0000

    fix eslint errors and modified fetchScore.js

--- src/modules/fetchScores.js
+++ src/modules/fetchScores.js
@@ -1,26 +1,27 @@
 import { url } from '../utils/url.js';
-import  render from './displayScore.js';
+import render from './displayScore.js';
 
 const fetchScores = async () => {
-    
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const scores = data.result;
-  
-      const uniqueScores = scores.reduce((acc, curr) => {
-        const existingScore = acc.find(score => score.user === curr.user);
-        if (existingScore) {
-          existingScore.score = Math.max(existingScore.score, curr.score);
-        } else {
-          acc.push(curr);
-        }
-        return acc;
-      }, []);
-      render(uniqueScores);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  export default fetchScores;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const scores = data.result;
+
+    const uniqueScores = scores.reduce((acc, curr) => {
+      const existingScore = acc.find((score) => score.user === curr.user);
+      if (existingScore) {
+        existingScore.score = Math.max(existingScore.score, curr.score);
+      } else {
+        acc.push(curr);
+      }
+      return acc;
+    }, []);
+    render(uniqueScores);
+
+    return uniqueScores;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default fetchScores;
